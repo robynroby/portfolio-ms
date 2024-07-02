676155d38cd6c82af0a024aec61c8ddd502ed87b
robynroby <robinsonmwaura254@gmail.com>
2024-07-02 14:57:47 +0300

    fixed some bugs

--- src/components/WorkSliderBtns.jsx
+++ src/components/WorkSliderBtns.jsx
@@ -9,11 +9,11 @@ const WorkSliderBtns = (containerStyles, btnStyles, iconsStyles) => {
     const swiper = useSwiper();
 
     return (
-        <div className={containerStyles}>
-            <button className={btnStyles} onClick={() => swiper.slidePrev()}>
+        <div className="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none">
+            <button className="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] flex justify-center items-center transition-all" onClick={() => swiper.slidePrev()}>
                 <PiCaretLeftBold className={iconsStyles} />
             </button>
-            <button className={btnStyles} onClick={() => swiper.slideNext()}>
+            <button className="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] flex justify-center items-center transition-all p-2" onClick={() => swiper.slideNext()}>
                 <PiCaretRightBold className={iconsStyles} />
             </button>
         </div>
