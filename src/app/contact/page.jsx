fb8005484f8bfbb30be2aef2804140b6900a053b
robynroby <robinsonmwaura254@gmail.com>
2024-07-15 18:07:01 +0300

    few changes

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -43,16 +43,16 @@ const Contact = () => {
                 <div className="flex flex-col xl:flex-row gap-[30px]">
                     {/* form */}
                     <div className="xl:w-[54%] order-2 xl:order-none">
-                        <form action="https://api.getform.io/v1/forms/8ea5fd1a-a99d-4cb2-a93c-da9d466cef61?token=89o29Ba7thqpd1bA3toZOmz7uXyhhfiQdfq37xQPQQmroqOQTPFJTQqQl2I6" method="POST" className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+                        <form action="https://getform.io/f/8ea5fd1a-a99d-4cb2-a93c-da9d466cef61" method="POST" className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
                             <h3 className="text-4xl text-accent">Lets work together</h3>
                             <p className="text-white/60">Connect with Mwaura, a passionate Software Developer, for innovative  services and collaboration opportunities.</p>
 
                             {/* inputs */}
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                                <Input type="firstname" placeholder="Firstname" />
-                                <Input type="lastname" placeholder="Lastname" />
-                                <Input type="email" placeholder="Email" />
-                                <Input type="phone" placeholder="Phone Number" />
+                                <Input type="firstname" placeholder="Firstname" name="firstname"/>
+                                <Input type="lastname" placeholder="Lastname" name="lastname" />
+                                <Input type="email" placeholder="Email" name="email" />
+                                <Input type="phone" placeholder="Phone Number" name="phonenumber" />
                             </div>
                             {/* select */}
                             <Select>
